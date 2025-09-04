aa114b79bbe0d04ee9fd6e95170ac11ffc3b1558
miki <mikiwork723@gmail.com>
2025-09-04 22:32:18 +0500

    настроил логирование

--- project2/main.py
+++ project2/main.py
@@ -5,3 +5,8 @@ import tempfile
 from telegram import Update
 from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
 import logging
+
+
+# Настройка логирования
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
