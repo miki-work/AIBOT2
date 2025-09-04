9d9cf8b4576728e28b4e4f0fb84049929490debf
miki <mikiwork723@gmail.com>
2025-09-04 22:36:19 +0500

    настроил .env и ollama api

--- project2/main.py
+++ project2/main.py
@@ -10,3 +10,12 @@ import logging
 # Настройка логирования
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
+
+#токен из .env
+TOKEN = os.getenv("TOKEN")
+if not TOKEN:
+    raise ValueError("Токен не найден! Убедись, что файл .env существует и содержит TOKEN.")
+
+# Ollama API
+OLLAMA_API = "http://host.docker.internal:11434/api/generate"
+MODEL = "llava"
